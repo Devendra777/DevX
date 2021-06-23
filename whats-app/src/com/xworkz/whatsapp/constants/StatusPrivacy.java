634de323074912887fecab404ca092e6be5cb958
Devendra777 <devendrasignh77@gmail.com>
2021-06-23 08:43:58 +0530

    committing arrays example

--- whats-app/src/com/xworkz/whatsapp/constants/StatusPrivacy.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.whatsapp.constants;
-
-public enum StatusPrivacy {    
-	
-	             MY_CONTACTS, EXCEPT_MY_CONTACTS , ONLY_SHARE_WITH
-	
-	
-
-}
--- whats-app/src/com/xworkz/whatsapp/service/WhatsAppService.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.whatsapp.service;
-
-import com.xworkz.whatsapp.dto.WhatsAppDTO;
-
-public interface WhatsAppService {
-	
-	public void validateAndCreateWhatsApp(WhatsAppDTO appDTO);
-
-}
